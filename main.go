ca0cb34e5adf639f34475cee004db0c489f85ea2
nssuperx <40332687+nssuperx@users.noreply.github.com>
2023-08-06 00:25:32 +0900

    できた。エラー処理とかはしてない。

--- main.go
+++ main.go
@@ -1,9 +1,56 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
+type Subtitles struct {
+	Body struct {
+		Div struct {
+			Text []string `xml:"p"`
+		} `xml:"div"`
+	} `xml:"body"`
+}
+
 func main() {
-	fmt.Println("hello")
+	videoId := os.Args[1]
+
+	_, serr := os.Stat(videoId + ".ja.ttml")
+	if serr != nil {
+		dlSub(videoId)
+	}
+	f, err := os.ReadFile(videoId + ".ja.ttml")
+	if err != nil {
+		panic(err)
+	}
+
+	result := Subtitles{}
+
+	err = xml.Unmarshal([]byte(string(f)), &result)
+	if err != nil {
+		panic(err)
+	}
+
+	outText := strings.Join(result.Body.Div.Text, "")
+
+	err = os.WriteFile(videoId+".txt", []byte(outText), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func dlSub(videoId string) {
+	out, err := exec.Command("powershell", ".\\yt-dlp.exe", "--write-sub", "--sub-lang", "ja", "--sub-format", "ttml", "--skip-download", "--id", videoId).Output()
+	if err != nil || strings.Contains(string(out), "There are no subtitles for the requested languages") {
+		fmt.Println("自動生成字幕を取得します。")
+		_, err = exec.Command("powershell", ".\\yt-dlp.exe", "--write-auto-sub", "--sub-lang", "ja", "--sub-format", "ttml", "--skip-download", "--id", videoId).Output()
+	}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
