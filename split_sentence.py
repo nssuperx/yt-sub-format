0a1d31b5209217bf0db044b06ec7c49a8c58cc23
nssuperx <40332687+nssuperx@users.noreply.github.com>
2023-08-13 18:59:43 +0900

    適当に改行を無視した

--- split_sentence.py
+++ split_sentence.py
@@ -37,39 +37,47 @@ def load_sub(video_id: str):
     with open(f"{video_id}.ja.json3", encoding="utf-8") as f:
         subtitle_json = json.load(f)
 
-    oneline: list[str] = []
-    for event in subtitle_json["events"]:
-        if "segs" in event:
-            for seg in event["segs"]:
-                oneline.append(seg.get("utf8", ""))
-
-    return "".join(oneline)
+    return [e["segs"] for e in subtitle_json["events"] if "segs" in e]
 
 
-def preprocess(text: str) -> str:
+def preprocess(events: list[dict]) -> str:
+    # この処理のところでいろいろやる
+    text = []
+    ignore_newline = False
+    for event in events:
+        segs_text = "".join(t["utf8"] for t in event)
+        if len(segs_text) > 17 and segs_text != "\n":
+            ignore_newline = True
+        if segs_text == "\n" and ignore_newline:
+            ignore_newline = False
+            continue
+        text.append(segs_text)
+
+    # text = "".join(utf8["utf8"] for event in events for utf8 in event)
+    text = "".join(text)
     processed_text = text
+
     # 単語以外で必要ないもの
-    unnecessary_str: tuple[str] = ("[音楽]", "\n")
+    # unnecessary_str: tuple[str] = ("[音楽]", "\n")
+    unnecessary_str: tuple[str] = ("[音楽]\n", "[音楽]")
     for s in unnecessary_str:
         processed_text = processed_text.replace(s, "")
 
-    # 間投詞, 感嘆詞
-    interjection: tuple[str] = ("なんか", "あの", "えー", "えっと")
-    for target in interjection:
-        processed_text = processed_text.replace(target, "")
     return processed_text
 
 
-def main():
-    video_id: str = sys.argv[1]
+def remove_interjection(text: str):
+    interjection: tuple[str] = ("なんか", "あの", "えー", "えっと")
+    for target in interjection:
+        text = text.replace(target, "")
+    return text
 
-    # video_info = dl_sub(video_id)
 
-    oneline = load_sub(video_id)
-    processed_text = preprocess(oneline)
-
+def split_sentence(processed_text: str):
     str_limit = 16500
-    split_text = [processed_text[x : x + str_limit] for x in range(0, len(processed_text), str_limit)]
+    split_text = [
+        processed_text[x : x + str_limit] for x in range(0, len(processed_text), str_limit)
+    ]
 
     nlp = spacy.load("ja_ginza")
 
@@ -81,11 +89,24 @@ def main():
     for doc in docs:
         for sent in doc.sents:
             sents.append(f"{sent.text}\n")
+    return sents
+
+
+def main():
+    video_id: str = sys.argv[1]
+
+    video_info = dl_sub(video_id)
+
+    events = load_sub(video_id)
+    processed_text = preprocess(events)
+
+    # processed_text = remove_interjection(processed_text)
+    # sents = split_sentence(processed_text)
 
     with open(f"{video_id}_sent.txt", encoding="utf-8", mode="w") as f:
-        f.writelines(sents)
+        f.writelines(processed_text)
 
-    print(f"文字数:{sum(list(map(lambda s: len(s), sents)))}")
+    print(f"文字数:{sum(len(s) for s in processed_text)}")
 
 
 if __name__ == "__main__":
