7d464bdff7b6a24c6c47432ccf84ab02ec3b7c27
nssuperx <40332687+nssuperx@users.noreply.github.com>
2023-10-29 14:33:03 +0900

    リファクタリング

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ YouTubeの字幕をダウンロードしていろいろする。
 .\yt-dlp.exe --write-auto-sub --sub-lang ja --sub-format ttml --skip-download <VideoId>
 .\yt-dlp.exe --write-sub --sub-lang ja --sub-format ttml --skip-download <VideoId>
 ```
+
+## やってること
+
+* 適当に文字列つなげて改行
+
+### 改行のやり方
+話者が話していない時に改行したい。
+
+元の字幕を見ると、改行されているタイミングは
+
+* 文字数が多くなってきたとき
+* 話者が話していないとき
+  * 改行か、`[音楽]`という文字列が入れられている
+
+上記の、「文字数が多くなってきたとき」だけ改行せずに次の字幕とつなげれば、大体、いい感じの文章になる。
--- split_sentence.py
+++ split_sentence.py
@@ -1,7 +1,8 @@
-import spacy
-import yt_dlp
-import json
+import os
 import sys
+from collections.abc import Generator, Iterable
+import xml.etree.ElementTree as ET
+import yt_dlp
 
 # https://github.com/yt-dlp/yt-dlp#embedding-yt-dlp
 
@@ -11,7 +12,7 @@ def dl_sub(video_id: str):
         "skip_download": True,
         "writesubtitles": True,
         "subtitleslangs": ["ja"],
-        "subtitlesformat": "json3",
+        "subtitlesformat": "ttml",
         "outtmpl": "%(id)s.%(ext)s",
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -23,7 +24,7 @@ def dl_sub(video_id: str):
             "skip_download": True,
             "writeautomaticsub": True,
             "subtitleslangs": ["ja"],
-            "subtitlesformat": "json3",
+            "subtitlesformat": "ttml",
             "outtmpl": "%(id)s.%(ext)s",
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -32,36 +33,40 @@ def dl_sub(video_id: str):
     return video_info
 
 
-def load_sub(video_id: str):
-    subtitle_json: dict = {}
-    with open(f"{video_id}.ja.json3", encoding="utf-8") as f:
-        subtitle_json = json.load(f)
+def load_sub(video_id: str) -> Generator[str]:
+    """ttmlファイルを読み込む
 
-    return [e["segs"] for e in subtitle_json["events"] if "segs" in e]
+    Args:
+        video_id (str): VideoID
+
+    Returns:
+        list[str]: 字幕1行が要素のgenerator
+    """
+    ns = {"ttml": "http://www.w3.org/ns/ttml"}
+    tree = ET.parse(f"{video_id}.ja.ttml")
+    root = tree.getroot()
+    return (subtitle.text for subtitle in root.findall("./ttml:body/ttml:div/ttml:p", ns))
 
 
-def preprocess(events: list[dict]) -> str:
-    # この処理のところでいろいろやる
-    text = []
-    ignore_newline = False
-    for event in events:
-        segs_text = "".join(t["utf8"] for t in event)
-        if len(segs_text) > 17 and segs_text != "\n":
-            ignore_newline = True
-        if segs_text == "\n" and ignore_newline:
-            ignore_newline = False
+def process_text(texts: Iterable[str]) -> list[str]:
+    """字幕の文字列(リスト)を適当に整形する
+
+    Args:
+        texts (Iterable[str]): 文字列を要素に持つイテラブルオブジェクト
+
+    Returns:
+        list[str]: 整形した文字列のリスト
+    """
+    processed_text: list[str] = []
+    oneline: list[str] = []
+    for text in texts:
+        if text == "[音楽]":
             continue
-        text.append(segs_text)
-
-    # text = "".join(utf8["utf8"] for event in events for utf8 in event)
-    text = "".join(text)
-    processed_text = text
-
-    # 単語以外で必要ないもの
-    # unnecessary_str: tuple[str] = ("[音楽]", "\n")
-    unnecessary_str: tuple[str] = ("[音楽]\n", "[音楽]")
-    for s in unnecessary_str:
-        processed_text = processed_text.replace(s, "")
+        oneline.append(text)
+        if len(text) > 17:
+            continue
+        processed_text.append("".join(oneline))
+        oneline = []
 
     return processed_text
 
@@ -73,38 +78,17 @@ def remove_interjection(text: str):
     return text
 
 
-def split_sentence(processed_text: str):
-    str_limit = 16500
-    split_text = [
-        processed_text[x : x + str_limit] for x in range(0, len(processed_text), str_limit)
-    ]
-
-    nlp = spacy.load("ja_ginza")
-
-    docs = []
-    for text in split_text:
-        docs.append(nlp(text))
-
-    sents: list[str] = []
-    for doc in docs:
-        for sent in doc.sents:
-            sents.append(f"{sent.text}\n")
-    return sents
-
-
 def main():
     video_id: str = sys.argv[1]
 
-    video_info = dl_sub(video_id)
+    if not os.path.isfile(f"{video_id}.ja.ttml"):
+        dl_sub(video_id)
 
-    events = load_sub(video_id)
-    processed_text = preprocess(events)
-
-    # processed_text = remove_interjection(processed_text)
-    # sents = split_sentence(processed_text)
+    texts = load_sub(video_id)
+    processed_text = process_text(texts)
 
     with open(f"{video_id}_sent.txt", encoding="utf-8", mode="w") as f:
-        f.writelines(processed_text)
+        f.write("\n".join(processed_text))
 
     print(f"文字数:{sum(len(s) for s in processed_text)}")
 
